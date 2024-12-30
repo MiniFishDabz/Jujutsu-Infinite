50487b28508b298e5d1e55f26841617950e2be1a
MiniFishDabz <149769887+MiniFishDabz@users.noreply.github.com>
2024-12-30 15:02:29 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MiniFishDabz/32444ee20abdc027cf2b0fc737cd287c/raw/2a4e1594382f44cd5c6be403b6758245587f5a49/JuJutsu%2520Infinite%2520Script.lua", true))()
