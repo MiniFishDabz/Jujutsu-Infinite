a1bd7f7827e80eb6b242bdf4bd3309d13351e88c
MiniFishDabz <149769887+MiniFishDabz@users.noreply.github.com>
2024-12-30 20:37:18 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/MiniFishDabz/32444ee20abdc027cf2b0fc737cd287c/raw/2a4e1594382f44cd5c6be403b6758245587f5a49/JuJutsu%2520Infinite%2520Script.lua", true))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/MiniFishDabz/32444ee20abdc027cf2b0fc737cd287c/raw/19aa0f74573be2f443c8d3e04491fa6dcaaea4dd/JuJutsu%2520Infinite%2520Script.lua", true))()
